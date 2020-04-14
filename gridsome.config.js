af9f388c2619c5f2b0c91dd43f6ef953878bb795
Trevor Clarke <besnoid@gmail.com>
2020-04-14 15:08:13 -0700

    testing forestry.io

---
+++ gridsome.config.js
@@ -0,0 +1,31 @@
+const config = require("./src/assets/content/data/config.json")
+const infoData = require("./src/assets/content/data/info.json")
+
+module.exports = {
+  siteName: config.title,
+  //need this for forestry preview docker image
+  host: process.env.HOSTNAME,
+  port: 8080,
+  //----------------
+  //make editable config & data files available to graphql
+  metaData: {
+    siteName: config.title,
+    siteDescription: config.description,
+    infoData: infoData, 
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Blog',
+        baseDir: "./src/assets/content/",
+        path: 'blog/**/*.md',
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    }
+  ]
+}
